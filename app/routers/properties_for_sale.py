6dbcfbce4e6727100df2b300f499a54322de60b0
Hafiz Umair <hafizumair.ie@gmail.com>
2024-04-27 15:48:57 +0500

    add proxies

--- app/routers/properties_for_sale.py
+++ app/routers/properties_for_sale.py
@@ -6,6 +6,20 @@ from schemas.properties import PropertiesForSale
 
 router = APIRouter()
 
+import random
+file = open('routers/file.txt', 'r')
+proxies = ['{}:{}@{}:{}'.format(v.split(':')[2], v.split(':')[3].strip(), v.split(':')[0], v.split(':')[1]) for v in file.readlines() if v.strip()]
+file.close()
+
+proxy = random.choice(proxies)
+proxies = {
+    'http':'http://{}'.format(proxy),
+    'https':'http://{}'.format(proxy)
+}
+
+
+
+
 
 @router.post("", status_code=status.HTTP_200_OK)
 async def properties_for_sale(params:PropertiesForSale):
@@ -28,7 +42,7 @@ async def properties_for_sale(params:PropertiesForSale):
     priceMin = params.priceMin
     radius = params.radius
     propertySubType = params.propertySubType
-    url = 'https://www.zoopla.co.uk/api/search/mobile/?added=3_days&bedsMax=10&bedsMin=1&includeRetirementHomes=true&includeSharedOwnership=true&includeSold=true&isAuction=true&location.identifier=london&location.value=London&newHomes=only&page=1&priceMax=15000000&priceMin=10000&propertySubType=park_home%2Cbungalow%2Cfarms_land%2Cflats%2Cterraced%2Csemi_detached&radius=0.25&section=for-sale&sortOrder=newest_listings'
+    url = f'https://www.zoopla.co.uk/api/search/mobile/?added={added}&bedsMax={bed_max}&bedsMin={bed_min}&includeRetirementHomes={includeRetirementHomes}&includeSharedOwnership={includeSharedOwnership}&includeSold={includeSold}&isAuction={isAuction}&location.identifier={location_identifier}&location.value={location_value}&newHomes={newHomes}&page={page}&priceMax={priceMax}&priceMin={priceMin}&propertySubType={propertySubType}&radius={radius}&section=for-sale&sortOrder=newest_listings'
     headers = {
         'x-api-key': 'ipqd2r4biqve2yh49sjxaujvpppey8s',
         'Host': 'www.zoopla.co.uk',
@@ -36,5 +50,32 @@ async def properties_for_sale(params:PropertiesForSale):
         'Accept-Encoding': 'gzip',
         'User-Agent': 'okhttp/4.9.2'
     }
-    resp = requests.get(url=url, headers=headers, verify=False, timeout=10)
-    return JSONResponse({"data": json.loads(resp.text)['data']}, status_code=status.HTTP_200_OK)
+    from base64 import b64decode
+
+    import requests
+
+    api_response = requests.post(
+        "https://api.zyte.com/v1/extract",
+        auth=("fa3af43178494478b7b971f0076e4d38", ""),
+        verify=False,
+        json={
+            "url": url,
+            "httpResponseBody": True,
+            "customHttpRequestHeaders": [
+                {
+                    "name": 'x-api-key',
+                    "value": 'ipqd2r4biqve2yh49sjxaujvpppey8s',
+                },
+                {
+                    "name": 'Host',
+                    "value": 'https://www.zoopla.co.uk',
+                },
+                {
+                    "name": 'User-Agent',
+                    "value": 'okhttp/4.9.2',
+                },
+            ]
+        }
+    )
+    
+    return JSONResponse({"data": json.loads(api_response.text)}, status_code=status.HTTP_200_OK)
